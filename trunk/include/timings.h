db944a86c9095f0b410e4b25a2892e4d6f9185a8
root <root@771786e7-7935-0410-82ac-f3570de231c4>
2007-07-19 21:10:31 +0000

    remove

--- trunk/include/timings.h
+++
@@ -1,8 +0,0 @@
-#include <sys/time.h>
-//#include <stdlib.h>
-#include <unistd.h>
-
-//typedef struct timeval CLOCK_T;
-#define CLOCK(c) gettimeofday(&c,(struct timezone *)NULL)
-#define CLOCK_DIFF(c1,c2) ((double)(c1.tv_sec-c2.tv_sec)+(double)(c1.tv_usec-c2.tv_usec)/1e+6)
-//#define CLOCK_DISPLAY(c) printf("sec: %d usec: %d\n",(int)c.tv_sec,(int)c.tv_usec)
